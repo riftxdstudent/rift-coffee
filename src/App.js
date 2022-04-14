321b0ef3d836e342ba05a4e8080f3e5ff8a98821
riftxdstudent <muhammad.rifqi2@student.umn.ac.id>
2022-04-14 12:51:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import About from './components/about/About';
+import Intro from './components/intro/Intro';
+import 'animate.css';
+import Market from './components/market/Market';
+
+function App() {
+  return (
+    <div className="App">
+     <Intro></Intro>
+     <About></About>
+     <Market></Market>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/market/Market.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Coffee from '../../content/image/coffee3.jpg'
+import './Market.css'
+import 'animate.css'
+
+const Market = () => {
+  return (
+    <div className='m'>
+        
+    </div>
+  )
+}
+
+export default Market
