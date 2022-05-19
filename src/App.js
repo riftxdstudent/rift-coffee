bf42a86cf3025a4d4ba6c14c5f16c214afe159c9
riftxdstudent <muhammad.rifqi2@student.umn.ac.id>
2022-05-19 22:16:33 +0700

    add Contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './components/about/About';
 import Intro from './components/intro/Intro';
 import 'animate.css';
 import Market from './components/market/Market';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Intro></Intro>
      <About></About>
      <Market></Market>
+     <Contact></Contact>
     </div>
   );
 }
