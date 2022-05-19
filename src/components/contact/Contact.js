77da8a2d834db04da5685b87bc87ce7b1d115983
riftxdstudent <muhammad.rifqi2@student.umn.ac.id>
2022-05-19 22:16:54 +0700

    Add Contact

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='c'>
+        <div className='c-left'>kiri</div>
+        <div className='c-right'>kanan</div>
+    </div>
+  )
+}
+
+export default Contact
